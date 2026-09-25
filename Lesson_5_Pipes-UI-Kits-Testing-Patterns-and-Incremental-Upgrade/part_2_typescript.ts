6017863efc000f3f1519a3c93baa7837873108ef
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2026-09-25 13:35:09 -0400

    Add Lesson_5_Pipes-UI-Kits-Testing-Patterns-and-Incremental-Upgrade/part_2_typescript.ts

---
+++ Lesson_5_Pipes-UI-Kits-Testing-Patterns-and-Incremental-Upgrade/part_2_typescript.ts
@@ -0,0 +1,21 @@
+import {Component } from '@angular/core';
+
+
+
+@Component({
+
+  template: `
+
+    <h5>Chain Pipes</h5>
+
+    <p>Month is {{today | date | uppercase | slice:0:4}}</p>
+
+  `,
+
+})
+
+export class ChainPipeComponent {
+
+  today = new Date();
+
+}
